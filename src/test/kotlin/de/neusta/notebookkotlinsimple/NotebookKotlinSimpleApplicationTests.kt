39b337ead151b4e88ba974113f0d71b756b7dbd7
KaiRenken <kai.renken@gmx.de>
2023-04-18 08:56:09 +0200

    initial commit

---
+++ src/test/kotlin/de/neusta/notebookkotlinsimple/NotebookKotlinSimpleApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.neusta.notebookkotlinsimple
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotebookKotlinSimpleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
