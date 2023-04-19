eb0cc7a6363d85a676524cc301bf705b5a5d5db4
KaiRenken <kai.renken@gmx.de>
2023-04-19 07:54:50 +0200

    second commit

--- src/main/kotlin/de/neusta/notebookkotlinsimple/infrastructure/repository/model/NoteEntity.kt
+++ src/main/kotlin/de/neusta/notebookkotlinsimple/infrastructure/repository/model/NoteEntity.kt
@@ -2,10 +2,12 @@ package de.neusta.notebookkotlinsimple.infrastructure.repository.model
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.time.LocalDateTime
 import java.util.*
 
 @Entity
+@Table(name = "note")
 class NoteEntity(
 
     @Id
